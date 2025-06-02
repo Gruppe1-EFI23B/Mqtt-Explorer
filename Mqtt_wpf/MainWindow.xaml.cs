b7c92bee181453fb4dc0cbe95c10dd8b36a25566
Loris <loris.zeljic@bbs2.schule>
2025-06-02 21:50:17 +0200

    Änderung in der Datenbankanbindung

--- Mqtt_wpf/MainWindow.xaml.cs
+++ Mqtt_wpf/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Data;
+using System.Data.Entity;
 using System.Windows;
+using Datenbank;
 using Mqtt_Explorer.ViewModels;
+using Microsoft.Data.Sqlite;
 
 namespace Mqtt_Explorer
 {
@@ -14,8 +18,36 @@ namespace Mqtt_Explorer
             InitializeComponent();
             // Setze das DataContext auf das ViewModel
             this.DataContext = new MqttViewModel();
-        }
+            try
+            {
+                var sql = @"SELECT * FROM Topics";
+                using var connection = new SqliteConnection("Data Source=C:/Users/loris/source/repos/Mqtt-Explorer/Datenbank.db");
+                connection.Open();
+                using var command = new SqliteCommand(sql, connection);
 
+                using var reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        var id = reader.GetInt32(0);
+                        var topic = reader.GetString(1);
+                        Console.WriteLine($"{id}\t{topic}");
+                        cbTopics.Items.Add(topic);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No authors found.");
+                }
+            }
+            catch (SqliteException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+                
+        }
+  
         // Zugriff auf das ViewModel über das DataContext
         private MqttViewModel Vm => (MqttViewModel)this.DataContext;
     }
